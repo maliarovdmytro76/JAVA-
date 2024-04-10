a904e920ae28c377ef98c8b72bb8e8acf21afce4
maliarovdmytro76 <160162226+maliarovdmytro76@users.noreply.github.com>
2024-04-10 20:20:44 +0200

    Add files via upload
    Homework 1-2

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello,");
-        System.out.println("my");
-        System.out.println("name");
-        System.out.println("is");
-        System.out.println("!");
+        System.out.print("Hello, ");
+        System.out.print("my ");
+        System.out.print("name ");
+        System.out.print("is ");
+        System.out.print("!");
     }
 }
